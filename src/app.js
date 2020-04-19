21ca5537b21a5ce9ec013b69c8c4feae1235e432
Joe Moore <joe.yobo@gmail.com>
2020-04-19 16:26:35 +0100

    Final Version

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import WhatToDo from './components/WhatToDo';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 
 ReactDOM.render(<WhatToDo />, document.getElementById('app'));
